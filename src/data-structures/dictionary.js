825dcefd7da13a720cbd241828fc44108717afb7
George <zxj@qq.com>
2020-12-31 11:41:03 +0800

    dictionary

---
+++ src/data-structures/dictionary.js
@@ -0,0 +1,55 @@
+/**
+ * 返回任意类型的字符串形式
+ * @param {any} item 
+ */
+function defaultToString (item) {
+  if (item === null) {
+    return 'NULL'
+  } else if (item === undefined) {
+    return 'UNDEFINED'
+  } else if (typeof item === 'string' || item instanceof String) {
+    return `${item}`
+  }
+  return item.toString()
+}
+
+class ValuePair {
+  constructor(key, value) {
+    this.key = key
+    this.value = value
+  }
+
+  toString () {
+    return `${this.key}: ${this.value}`
+  }
+}
+
+class Dictionary {
+  constructor(toStrFn = defaultToString) {
+    this.toStrFn = toStrFn
+    this.table = {}
+  }
+
+  hasKey (key) {
+    return this.table[this.toStrFn[key]] != null
+  }
+
+  set (key, value) {
+    if (key != null && value != null) {
+      const tableKey = this.toStrFn(key)
+      this.table[tableKey] = new ValuePair(key, value)
+      return true
+    }
+
+    return false
+  }
+
+  remove (key) {
+    if (this.hasKey(key)) {
+      Reflect.defineProperty(this.table, this.toStrFn(key))
+      return true
+    }
+
+    return false
+  }
+}
